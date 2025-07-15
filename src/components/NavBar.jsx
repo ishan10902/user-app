7d1dd879fb9be0d6b17ba9d4a9ea86e44a6552e7
ishan10902 <ishansugath@gmail.com>
2025-07-15 23:24:39 +0530

    complete

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg" style={{ backgroundColor: '#003366' }}>
                 <div class="container-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -12,16 +12,16 @@ const NavBar = () => {
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/">Add User</Link>
+                                <Link class="nav-link active" aria-current="page" to="/" style={{ color: 'white' }}>Add User</Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="/search">Search</Link>
+                                <Link class="nav-link" to="/search" style={{ color: 'white' }}>Search</Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="/delete">Delete</Link>
+                                <Link class="nav-link" to="/delete" style={{ color: 'white' }}>Delete</Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="/view">View All</Link>
+                                <Link class="nav-link" to="/view" style={{ color: 'white' }}>View All</Link>
                                 
                             </li>
                         </ul>
